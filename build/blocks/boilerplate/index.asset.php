bf85e9e7db5435443085debef8fef51f6116d8d4
ZakariaBDT <zakaria@bdthemes.com>
2023-05-08 17:08:18 +0600

    compnent error is removed

--- build/blocks/boilerplate/index.asset.php
+++ build/blocks/boilerplate/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd1e56ed70b257bd5d877');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7896dd4662d060f25817');
